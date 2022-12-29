947ce1354692b551d1272fa50fc9f3cbdc8bd3b9
Sergio <sergio.troncoso@noirlab.edu>
2022-12-28 21:37:56 -0300

    Fix issues. Working hook

--- build/lib/check_headers/main.py
+++ build/lib/check_headers/main.py
@@ -1,14 +1,13 @@
 # Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
 # For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-from pathlib import Path
-
-from definitions import ROOT_DIR
+import argparse
 
 # Checks the headers of all .py files to make sure that they contain the above Copyright message.
 
 
 HEADER = '# Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)\n# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause'
+
 
 def pre_adder(file):
     with open(file, 'r+') as f:
@@ -17,14 +16,19 @@ def pre_adder(file):
         f.write(HEADER + '\n' + content)
 
 
-if __name__ == '__main__':
+def main():
     COPYRIGHT = '# Copyright'
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args()
+
     bad_files = []
-    for path in Path(ROOT_DIR).rglob('*.py'):
-        with open(path, 'r') as f:
-            if not f.readline().startswith(COPYRIGHT):
-                bad_files.append(path)
+    for path in args.filenames:
+        if path.endswith('.py'):
+            with open(path, 'r') as f:
+                if not f.readline().startswith(COPYRIGHT):
+                    bad_files.append(path)
 
     if bad_files:
         print('*** FILES MISSING COPYRIGHT ***')
@@ -35,3 +39,7 @@ if __name__ == '__main__':
     else:
         print('All files copyrighted.')
         exit(0)
+
+
+if __name__ == '__main__':
+    main()
